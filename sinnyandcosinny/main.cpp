aff8d7c6669f2e2f0ae192eba5acb1b9c3ed0e65
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2022-02-03 16:26:26 -0800

    Initial Commit

---
+++ sinnyandcosinny/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sinnyandcosinny
+//
+//  Created by Abel Christy Anand on 2/3/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
